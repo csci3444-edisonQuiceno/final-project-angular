7548f533ca5ef0420dee38377fb0c2ff4d230b24
edisonQuiceno <equiceno@student.fdu.edu>
2017-12-25 09:12:33 -0500

    edited app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TeacherListComponent } from './components/teacher-list/teacher-list.component';
-
+import { TeacherListDataService } from './services/teacher-list-data.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +12,10 @@ import { TeacherListComponent } from './components/teacher-list/teacher-list.com
     TeacherListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule      // Added for TeacherListDataService to make REST call
   ],
-  providers: [],
+  providers: [TeacherListDataService],  // Added TeacherListDataService after creating the service via CLI
   bootstrap: [AppComponent]
 })
 export class AppModule { }
