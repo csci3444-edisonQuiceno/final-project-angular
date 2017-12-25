afa040abea1dd747da762b3d217756e6b550c2bd
edisonQuiceno <equiceno@student.fdu.edu>
2017-12-25 00:35:50 -0500

    Initial Setup - Created Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TeacherListComponent } from './components/teacher-list/teacher-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherListComponent
   ],
   imports: [
     BrowserModule
